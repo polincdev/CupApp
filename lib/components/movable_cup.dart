f96bc07a2ef5db5705332bc6265a2c967fd34823
polincdev <polincgames@gmail.com>
2022-02-16 10:14:01 +0100

    Update

--- lib/components/movable_cup.dart
+++ lib/components/movable_cup.dart
@@ -8,37 +8,44 @@ class MovableCup  {
   late AnimatedPositioned positioned;
   double left=-1, right=-1, top=-1, bottom=-1;
   int index=-1;
-
+  double scale=1;
   MovableCup(Function refresh, Size size, double iconWidth, String image, String pos){
 
     if(pos=="topLeft"){
-      left=0; top=0;
+      left=0;
+      top=0;
       index=0;
     }
    else  if(pos=="topCenter"){
-      left=(size.width/2-iconWidth/2-kDefaultPadding); top=0;
+      left=(size.width/2-iconWidth/2-kDefaultPadding);
+      top=0;
       index=1;
     }
     else  if(pos=="topRight"){
-      right=0; top=0;
+      left=(size.width-iconWidth-kDefaultPadding*2);
+      top=0;
       index=2;
     }
     if(pos=="bottomLeft"){
-      left=0; bottom=0;
+      left=0;
+      top=(size.height-iconWidth*2-kDefaultPadding);
       index=3;
     }
     else  if(pos=="bottomCenter"){
-      left=(size.width/2-iconWidth/2-kDefaultPadding); bottom=0;
+      left=(size.width/2-iconWidth/2-kDefaultPadding);
+      top=(size.height-iconWidth*2-kDefaultPadding);
       index=4;
     }
     else  if(pos=="bottomRight"){
-      right=0; bottom=0;
+      left=(size.width-iconWidth-kDefaultPadding*2);
+      top=(size.height-iconWidth*2-kDefaultPadding);
       index=5;
     }
     else  if(pos=="center"){
-      top=0; bottom=0;
-       left=0;
-       right=0;
+      scale=scale*2;
+      top=(size.height/2-iconWidth/2-kDefaultPadding);
+      left=(size.width/2-iconWidth/2-kDefaultPadding);
+
       index=6;
     }
     positioned=AnimatedPositioned(duration: Duration(seconds: 1),
@@ -46,7 +53,7 @@ class MovableCup  {
         top:top>-1?top:null,
         right:right>-1?right:null,
         bottom:bottom>-1?bottom:null,
-        child:    GestureDetector(onTap: (){refresh(index);}, child: Image.asset("assets/images/pngsmall/"+image, width:iconWidth  )));
+        child:    GestureDetector(onTap: (){refresh(index);}, child: AnimatedScale(duration: Duration(seconds: 1), scale:scale,child: Image.asset("assets/images/pngsmall/"+image, width:iconWidth  ))));
 
 
 
