85c74070d43340488144b474f6fe5624c00b410b
polincdev <polincgames@gmail.com>
2022-03-04 07:12:05 +0100

    Update

--- lib/screen/eight_screen.dart
+++ lib/screen/eight_screen.dart
@@ -17,133 +17,127 @@ class EightScreen extends StatefulWidget{
 
 }
 
-class EightScreenState  extends State<StatefulWidget>{
+class EightScreenState  extends State<StatefulWidget> with SingleTickerProviderStateMixin{
+  late int timeFrom;
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 100),
+    vsync: this,
+  )..repeat();
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    timeFrom= (DateTime.now().millisecondsSinceEpoch/1000).toInt();
+    print("dela="+timeFrom.toString());
+    super.initState();
 
-
-  Future<Shader> initShader() async {
-    try {
-      ByteBuffer spirvBuffer = await Utils.getFileDataFromAssets(
-          "assets/shaders/shader.sprv");
-      print("SHADER=" + spirvBuffer.toString());
-      FragmentProgram fraProg = await FragmentProgram.compile(
-          spirv: spirvBuffer, debugPrint: true);
-      Shader shader = fraProg.shader(/*
-        floatUniforms: Float32List.fromList(<double>[1]),*/);
-      return shader;
-    }
-    catch(e){print("errar="+e.toString());}
-
-    final Rect rect =  const Offset(1.0, 2.0) & const Size(3.0, 4.0);
-    const RadialGradient gradient = RadialGradient(
-      center: Alignment(0.7, -0.6),
-      radius: 0.2,
-      colors: <Color>[Color(0xFFFFFF00), Color(0xFF0099FF)],
-      stops: <double>[0.4, 1.0],
-    );
-
-
-    return gradient.createShader(rect);
   }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
+  Future<FragmentProgram> initShader() async {
+    late ByteBuffer spirvBuffer;
+    try {
+      spirvBuffer = await Utils.getFileDataFromAssets(    "assets/shaders/shader.sprv");
+      //  print("SHADER=" + spirvBuffer.toString());
+      FragmentProgram fraProg = await FragmentProgram.compile(
+          spirv: spirvBuffer, debugPrint: true);
+      return fraProg;
+    }
+    catch(e){print("errar="+e.toString());}
+    //TEST never reached
+    FragmentProgram fraProg = await FragmentProgram.compile(  spirv: spirvBuffer, debugPrint: true);
+    return fraProg;
+  }
   @override
   Widget build(BuildContext context) {
     Size size=MediaQuery.of(context).size;
    return Scaffold(
-     body:  Container( width: size.width,height: size.height  ,
-          child:
-
-
-          Container(
-            width:size.width,height:size.height*0.4,
-            child: Stack(
-                children:[
-                  Positioned(top:120,left:150,
-
-                      child:
-
-                      Container(   width:150,height:150,
-                             child:
-                           //  CustomPaint(painter: PathPainter())
-
-                             FutureBuilder(
-                               future: initShader(),
-                               builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                                 if(snapshot.connectionState==ConnectionState.waiting)
-                                   return CircularProgressIndicator();
-                                 else  if(snapshot.hasError)
-                                   return Text("Error "+snapshot.error.toString());
-                                 else
-                                  // return   CustomPaint(painter: PathPainter());
-                                   return CustomPaint(painter: ShaderPainter2(shader:snapshot.data as Shader)) ;
-                               },
-                             ),
-
-
-                  ),
-
-                  ) ,
-                ]
-            ),
-
-          )
-
-
-
-
-
+     body: FutureBuilder( //  CustomPaint(painter: PathPainter())
+       future: initShader(),
+       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+         if(snapshot.connectionState==ConnectionState.waiting)
+           return CircularProgressIndicator();
+         else  if(snapshot.hasError)
+           return Text("Error "+snapshot.error.toString());
+         else
+           return SpinningContainer(controller: _controller, fraProg:  snapshot.data as FragmentProgram, timeFrom: timeFrom,);
+          // return   CustomPaint(painter: PathPainter());
+       },
      ),
+
+
+
+
    );
   }
 }
 
+class SpinningContainer extends AnimatedWidget {
+  final FragmentProgram fraProg;
+  final AnimationController controller;
+  final int timeFrom;
+  const SpinningContainer({
+    Key? key,
+    required this.fraProg,
+    required this.timeFrom,
+    required this.controller,
+  }) : super(key: key, listenable: controller);
 
-class ShaderPainter extends CustomPainter {
+  Animation<double> get _progress => listenable as Animation<double>;
 
-  final Shader shader;
-  final Paint _paint;
-  ShaderPainter({required this.shader}):_paint=Paint()..shader=shader;
   @override
-  void paint(Canvas canvas, Size size) {
+  Widget build(BuildContext context) {
+    Size size=MediaQuery.of(context).size;
+    return
+      Transform.rotate(
+        angle:  0,// _progress.value * 2.0 * math.pi,
+        child:  Container( width: size.width,height: size.height  ,
+          child:    CustomPaint(painter: ShaderPainter2( size:size,fraProg:fraProg, timeFrom:  timeFrom)),
 
-    final angle = -math.pi / 4;
-    Color paintColor = Colors.orange.withOpacity(0.75);
-    Paint circlePaint = Paint()
-      ..color = paintColor
-      ..style = PaintingStyle.fill
-      ..strokeWidth = 5;
 
-    canvas.save();
-    // canvas.translate(100, 100);
-    canvas.rotate(angle);
-    canvas.drawOval(Rect.fromCenter(center: Offset.zero, width: 155, height: 170), circlePaint);
-    canvas.restore();
-
+        ),
+      );
   }
 
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
 
+
+
 class ShaderPainter2 extends CustomPainter {
-  final Shader shader;
-  final Paint _paint;
-  ShaderPainter2({required this.shader}):_paint=Paint() ;
+  final Size size;
+  Paint _paint = Paint();
+  final FragmentProgram fraProg;
+  final int timeFrom ;
+  ShaderPainter2( {required this.size,required this.fraProg, required this.timeFrom}){
+
+  }
 
 
   @override
   void paint(Canvas canvas, Size size) {
+    int delta=(DateTime.now().millisecondsSinceEpoch/1000).toInt()-timeFrom;
+    print("dela="+delta.toString());
+    Shader shader = fraProg.shader( floatUniforms: Float32List.fromList(
+        <double>[
+          timeFrom.toDouble(),
+          delta.toDouble(),//
+          size.width,
+          size.height,// Delta
+          0.1]), );
+    _paint.shader=shader;
+    // Color paintColor = Colors.blue.withOpacity(0.50);
+    //  _paint.color = paintColor;
 
-    Color paintColor = Colors.blue.withOpacity(0.50);
-
-    _paint.color = paintColor;
-     _paint.shader=shader;
-
-     canvas.drawPaint(_paint);
-    canvas.drawRect(const Rect.fromLTWH(0, 0, 100, 100), _paint);
+    canvas.drawPaint(_paint);
+    //canvas.drawRect(const Rect.fromLTWH(0, 0, 100, 100), _paint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
+
 }
